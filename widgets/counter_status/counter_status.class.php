01063cd56f2a94601a270b4e82cc47490ea14da6
BJRambo <sosifam@070805.co.kr>
2014-01-17 22:44:52 +0900

    if문 명확하게 하고, 로케이션 위치가 ~~
    if문 명확하게 하고, 로케이션 위치가 '?>' 문 밖에 위치하여 사이트의 접근이 불가능하던 현상 개선 시킴.

--- widgets/counter_status/counter_status.class.php
+++ widgets/counter_status/counter_status.class.php
@@ -6,7 +6,9 @@
      * @brief Display counter status by using data in the counter module
      **/
 
+
     class counter_status extends WidgetHandler {
+
 
         /**
          * @brief Widget execution
@@ -17,18 +19,18 @@
             // 전체, 어제, 오늘 접속 현황을 가져옴
             $oCounterModel = &getModel('counter');
 
+
             $site_module_info = Context::get('site_module_info');
             $output = $oCounterModel->getStatus(array('00000000', date('Ymd', $_SERVER['REQUEST_TIME']-60*60*24), date('Ymd')), $site_module_info->site_srl);
-			if(count($output))
-			{
-				foreach($output as $key => $val)
+            if(count($output) > 0)
+            {
+            foreach($output as $key => $val)
 				{
 					if(!$key) Context::set('total_counter', $val);
 					elseif($key == date("Ymd")) Context::set('today_counter', $val);
 					else Context::set('yesterday_counter', $val);
-				}
+                }
 			}
-
 			// 가입한 회원수 출력
 			$oMemberModel = &getModel('member');
             $args->date = date("Ymd000000", $_SERVER['REQUEST_TIME']-60*60*24);
@@ -48,6 +50,7 @@
             Context::set('start_module', $output->data);
             Context::set('status', $status);
 
+
             // 전체글수
             $output = executeQueryArray("admin.getDocumentStatus", $args);
             if($output->data) {
@@ -63,6 +66,7 @@
             $status->document->total = $output->data->count;
             Context::set('start_module', $output->data);
             Context::set('status', $status);
+
 
             // 전체 댓글수
             $output = executeQueryArray("admin.getCommentStatus", $args);
@@ -80,6 +84,7 @@
             Context::set('start_module', $output->data);
             Context::set('status', $status);
 
+
             // 엮인글수
             $output = executeQueryArray("admin.getTrackbackStatus", $args);
             if($output->data) {
@@ -95,6 +100,7 @@
             $status->trackback->total = $output->data->count;
             Context::set('start_module', $output->data);
             Context::set('status', $status);
+
 
             // 첨부파일수
             $output = executeQueryArray("admin.getFileStatus", $args);
@@ -112,19 +118,23 @@
             Context::set('start_module', $output->data);
             Context::set('status', $status);
 
+
             // Set a path of the template skin (values of skin, colorset settings)
             $tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
             Context::set('colorset', $args->colorset);
 
+
             // Specify a template file
             $tpl_file = 'counter_status';
+
 
             // Compile a template
             $oTemplate = &TemplateHandler::getInstance();
             return $oTemplate->compile($tpl_path, $tpl_file);
         }
     }
-?>
+	
 
 /* End of file counter_status.class.php */
 /* Location: ./widgets/counter_status/counter_status.class.php */
+?>
