c7612ca66ffb76a7d1526348a50160b8db92867f
bnu <bnufactory@gmail.com>
2014-02-12 20:46:37 +0900

    work

---
+++ work/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<title>점검중 입니다.</title> 
+<style type="text/css"> 
+html, body{ height:100%; margin:0;}
+.notice{ position:absolute; background:#ddd; text-align:center; left:50%; top:50%; padding:60px 0 0 0; width:500px; height:140px; margin:-100px 0 0 -250px; border:1px solid #ccc; box-shadow:0 0 10px silver; -moz-box-shadow:0 0 10px silver; -webkit-box-shadow:0 0 10px silver; filter:progid:DXImageTransform.Microsoft.Shadow(color=silver,direction=135, strength=5); text-shadow:1px 1px 0 #fff; filter:progid:DXImageTransform.Microsoft.dropshadow(OffX=1, OffY=1, Color=#ffffff, Positive=true); display:inline-block; zoom:1; border-radius:10px; -moz-border-radius:10px; -webkit-border-radius:10px;}
+.notice h1{ margin:0 0 20px 0; font-size:32px; font-weight:normal; font-family:나눔고딕, NanumGothic, "맑은 고딕", "Malgun Gothic", 돋움, Dotum, Tahoma, Geneva, sans-serif;}
+.notice p{ margin:0; font-size:14px; font-family:돋움, Dotum, Tahoma, Geneva, sans-serif;}
+</style> 
+</head> 
+<body> 
+<div class="notice"> 
+    <h1>서버 <strong>점검중</strong> 입니다.</h1> 
+    <p>2013.09.06 오후 2시 ~ 오후 7시</p> 
+</div> 
+</body> 
+</html> 
