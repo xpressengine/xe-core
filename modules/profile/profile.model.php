6a198c0cb1800b51836638ce52bb2e33ff98f0f3
bnu <bnufactory@gmail.com>
2014-04-03 13:00:15 +0900

    profile

--- modules/profile/profile.model.php
+++ modules/profile/profile.model.php
@@ -7,20 +7,45 @@ class profileModel extends profile
 	function _getProfile($member_srl)
 	{
 		$oMemberModel = &getModel('member');
+		$logged_info = Context::get('logged_info');
+
+		$profile = new stdClass;
+		$profile->member_srl = $member_srl;
+		$profile->is_owner = ($logged_info->member_srl == $member_srl) ? true : false;
 
 		$cond = new stdClass;
 		$cond->member_srl = $member_srl;
-		$profile = executeQuery('profile.getProfile', $cond)->data;
-
-		if(!$profile) {
-			$profile = new stdClass;
+		$result = executeQuery('profile.getProfile', $cond)->data;
+debugPrint($result);
+		if(!$result)
+		{
 			$profile->status = 'UNREGISTERED';
-		} else {
-			$profile->profile_image = $oMemberModel->getProfileImage($member_srl)->src;
-			$profile->signature = $oMemberModel->getSignature($member_srl);
+		}
+		else
+		{
+			$member = new stdClass;
+			$member->nick_name = $result->nick_name;
+			$member->user_name = $result->user_name;
+			$member->email_address = $result->email_address;
+			$member->limit_date = $result->limit_date;
+			$member->limit_date = $result->limit_date;
+			$member->profile_image = $oMemberModel->getProfileImage($member_srl)->src;
+			$member->signature = $oMemberModel->getSignature($member_srl);
+
+			$profile->type = $result->profile_type;
+			$profile->status = $result->status;
+			$profile->likes = $result->likes;
+			$profile->config = json_decode($result->config);
+			$profile->member = $member;
+
 		}
 
+		if($result->denied != 'N')
+		{
+			$profile->status = 'BLOCK';
+		}
 		debugPrint($profile);
+
 		// $status
 // UNREGISTERED = 미등록
 // PUBLIC = 공개
@@ -28,6 +53,14 @@ class profileModel extends profile
 // BLOCK = 차단
 
 		return $profile;
+	}
+
+	public function getListPackage($args)
+	{
+		$output = executeQueryArray('profile.getPackageList', $args);
+		if(!$output->data) $output->data = array();
+
+		return $output;
 	}
 
 	public function getTimeline($member_srl, $args = null)
@@ -43,6 +76,22 @@ class profileModel extends profile
 		foreach($output->data as $item) {
 			$item->metadata = json_decode($item->metadata);
 		}
+
+		return $output;
+	}
+
+	public function getPromotionBanner($args = null)
+	{
+		$output = executeQueryArray('profile.getPromotionBanner', $args);
+		if(!$output->data) $output->data = array();
+
+		return $output->data;
+	}
+
+	public function getShowcaseList($args = null)
+	{
+		$output = executeQueryArray('profile.getShowcaseList', $args);
+		if(!$output->data) $output->data = array();
 
 		return $output;
 	}
--- modules/profile/profile.view.php
+++ modules/profile/profile.view.php
@@ -12,24 +12,27 @@ class profileView extends profile
 		if(!$member_srl) {
 			$member_srl = Context::get('logged_info')->member_srl;
 		}
-		if(!$member_srl) return new Object(-1, '잘못된 접근');
-
-		$this->profile = new stdClass;
-		$this->profile->member_srl = $member_srl;
+		if(!$member_srl) return new Object(-1, '잘못된 접근입니다');
 
 		$oModel = getModel('profile');
-		$this->profile->member = $oModel->_getProfile($this->profile->member_srl);
-debugPrint('set');
+		$this->profile = $oModel->_getProfile($member_srl);
+		debugPrint($this->profile);
+
 		Context::set('profile', $this->profile);
 
 		$this->setTemplatePath($this->module_path . 'tpl/views');
 		$this->setTemplateFile(str_replace('disp', '', $this->act));
 
-		if($this->profile->member->status != 'PUBLIC') {
-			if($logged_info->member_srl == $member_srl) {
-				// if($this->act != 'dispProfileSetting') header('Location: ' . getNotEncodedFullUrl('act','dispProfileSetting'));
-			} else if($this->profile->member->status == 'NONE') {
+		if(!$this->profile->is_owner && $this->profile->status != 'PUBLIC')
+		{
+			if($this->profile->status == 'UNREGISTERED')
+			{
 				$this->stop('프로필을 등록하지 않은 회원입니다.');
+				return;
+			}
+			else if($this->profile->status == 'PRIVATE')
+			{
+				$this->stop('프로필을 공개하지 않은 회원입니다.');
 				return;
 			}
 		}
@@ -63,6 +66,17 @@ debugPrint('set');
 
 		$timeline = $oModel->getTimeline($member_srl);
 		Context::set('timeline', $timeline);
+
+		$cond = new stdClass;
+		$cond->member_srl = $member_srl;
+		$package_list = $oModel->getListPackage($cond)->data;
+		Context::set('package_list', $package_list);
+
+		$cond = new stdClass;
+		$cond->member_srl = $member_srl;
+		$showcase_list = $oModel->getShowcaseList($cond)->data;
+		Context::set('showcase_list', $showcase_list);
+
 	}
 
 	// 쇼케이스
