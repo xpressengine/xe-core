6ab290adb49dcaf6e500b9bd94299721a18b023b
bnu <bnufactory@gmail.com>
2014-04-10 11:09:16 +0900

    main

--- opages/xe_v4/main.php
+++ opages/xe_v4/main.php
@@ -1,19 +1,55 @@
 <div class="xev4-main">
-	<div class="download">
-		<a href="http://www.xpressengine.com/index.php?mid=download&nbsp;package_srl=18325662" class="link downloadXE">
-			DOWNLOAD
-			<span>XE Core 1.7.4.1</span>
-		</a>
-		<a href="https://github.com/xpressengine/xe-core" target="blank" class="btn">
-			Fork Me !
-			<span>XE Github</span>
-		</a>
+
+	<div class="row">
+		<div class="col-xs-5">
+			[visual]
+		</div>
+
+		<div class="col-xs-7">
+			<div>
+				<h3 class="sub-header">XpressEngine</h3>
+				<p class="lead">XE는 블라블라</p>
+			</div>
+			<div class="row">
+				<div clas="col-xs-5">
+					<a href="http://www.xpressengine.com/index.php?mid=download&nbsp;package_srl=18325662" class="link downloadXE btn btn-info btn-">
+						<strong><i class="fa fa-download fa-lg"></i> Download XE</strong>
+					</a>
+				</div>
+				<div clas="col-xs-7">
+					<iframe src="http://ghbtns.com/github-btn.html?user=xpressengine&repo=xe-core&type=watch&size=large&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="130" height="30" style="vertical-align:middle;"></iframe>
+					<iframe src="http://ghbtns.com/github-btn.html?user=xpressengine&repo=xe-core&type=fork&size=large&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="130" height="30" style="vertical-align:middle;"></iframe>
+					
+				</div>
+			</div>
+		</div>
 	</div>
 
-	<xmp>
-		포럼
-		blog
-		쇼케이스
-		자료실 - 최근 또는 기간 내 인기
-	</xmp>
+	<div class="row">
+		<div class="col-xs-6">
+			blog
+			<img class="zbxe_widget_output" widget="content" skin="default" colorset="white" content_type="document" module_srls="22567768" list_type="normal" tab_type="none" markup_type="list" page_count="1" option_view="title,regdate" show_browser_title="N" show_comment_count="N" show_trackback_count="N" show_category="N" show_icon="N" order_target="regdate" order_type="desc" thumbnail_type="crop" />
+		</div>
+		[visual]
+	</div>
+
+	<div class="row">
+		<div class="col-xs-6">
+			쇼케이스
+		</div>
+		<div class="col-xs-6">
+			자료실
+			<div class="row">
+				<div class="col-xs-4">
+					a
+				</div>
+				<div class="col-xs-4">
+					a
+				</div>
+				<div class="col-xs-4">
+					a
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
