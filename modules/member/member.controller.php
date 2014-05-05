79b8c25a918fee4d51a33ba43789bb89ecc4be71
KissToXE <clilawyer@naver.com>
2014-05-05 19:05:19 +0900

    user_name이 임의 숫자로 처리되는 현상 수정
    user_name이 임의 숫자로 처리되는 현상 수정

--- modules/member/member.controller.php
+++ modules/member/member.controller.php
@@ -1916,7 +1916,7 @@ class memberController extends member
 		if(!$args->user_id) $args->user_id = 't'.$args->member_srl;
 		// Enter the user's identity changed to lowercase
 		else $args->user_id = strtolower($args->user_id);
-		if(!$args->user_name) $args->user_name = $args->member_srl;
+		$args->user_name = htmlspecialchars(Context::get('user_name'));
 		if(!$args->nick_name) $args->nick_name = $args->member_srl;
 
 		// Control of essential parameters
@@ -1977,7 +1977,7 @@ class memberController extends member
 
 
 		if(!$args->user_id) $args->user_id = 't'.$args->member_srl;
-		if(!$args->user_name) $args->user_name = $args->member_srl;
+		$args->user_name = htmlspecialchars(Context::get('user_name'));
 
 		$oDB = &DB::getInstance();
 		$oDB->begin();
