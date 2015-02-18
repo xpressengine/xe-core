56ac991ed07185f688b2a637f848e91bacf686e5
조태상 <gsts007@naver.com>
2015-02-18 13:23:31 +0900

    잘못된 주석 수정

--- modules/point/point.admin.controller.php
+++ modules/point/point.admin.controller.php
@@ -49,7 +49,7 @@ class pointAdminController extends point
 		// Check if reading a document is not allowed
 		if($args->disable_read_document == 'Y') $config->disable_read_document = 'Y';
 		else $config->disable_read_document = 'N';
-		// Check if reading a document is not allowed
+		// Check if point negative is not allowed
 		if($args->enable_point_negative == 'Y') $config->enable_point_negative = 'Y';
 		else $config->enable_point_negative = 'N';
 
