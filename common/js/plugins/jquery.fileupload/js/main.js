rhymix/rhymix

이미지 파일 업로드시 본문 자동삽입되도록 수정

common/js/plugins/jquery.fileupload/js/main.js
@@ -93,6 +93,7 @@
 				},
 				done: function(e, res) {
 					var result = res.response().result;
+					var temp_code = '';
 
 					if(!result) return;
 
@@ -101,6 +102,12 @@
 					if(!result) return;
 
 					if(result.error == 0) {
+						if(/\.(jpe?g|png|gif)$/i.test(result.download_url)) {
+							temp_code += '<img src="' + window.request_uri + result.download_url + '" alt="' + result.source_filename + '" editor_component="image_link" data-file-srl="' + result.file_srl + '" />';
+							temp_code += "\r\n<p><br></p>\r\n";
+						}
+
+						_getCkeInstance(settings.formData.editor_sequence).insertHtml(temp_code, "unfiltered_html");
 					} else {
 						alert(result.message);
 					}

xpressengine/xe-core

common/js/plugins/jquery.fileupload/js/main.js
@@ -93,6 +93,7 @@
 				},
 				done: function(e, res) {
 					var result = res.response().result;
+					var temp_code = '';
 
 					if(!result) return;
 
@@ -101,6 +102,12 @@
 					if(!result) return;
 
 					if(result.error == 0) {
+						if(/\.(jpe?g|png|gif)$/i.test(result.source_filename)) {
+							temp_code += '<img src="' + window.request_uri + result.download_url + '" alt="' + result.source_filename + '" editor_component="image_link" data-file-srl="' + result.file_srl + '" />';
+							temp_code += "\r\n<p><br></p>\r\n";
+						}
+
+						_getCkeInstance(settings.formData.editor_sequence).insertHtml(temp_code, "unfiltered_html");
 					} else {
 						alert(result.message);
 					}
@@ -236,7 +243,7 @@
 
 				if(!fileinfo) return;
 
-				if(/\.(jpe?g|png|gif)$/i.test(fileinfo.download_url)) {
+				if(/\.(jpe?g|png|gif)$/i.test(fileinfo.source_filename)) {
 					temp_code += '<img src="' + window.request_uri + fileinfo.download_url + '" alt="' + fileinfo.source_filename + '" editor_component="image_link" data-file-srl="' + fileinfo.file_srl + '" />';
 					temp_code += "\r\n<p><br></p>\r\n";
 				} else {