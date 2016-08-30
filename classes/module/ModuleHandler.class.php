74244c27b43a3a46a5308dfa0157021b65efe0ea
bnu <bnufactory@gmail.com>
2016-08-30 12:17:10 +0900

    #1951 HTTP status code 목록이 잘못 작성된 문제 수정 - 200 Ok가 201로 작성된 문제 고침 - 목록에 없는 코드 추가 - 목록에 없는 코드의 메시지 반환 시 'OK'로 고정되어 있던 것을 'HTTP xxx'으로 반환하도록 변경

--- classes/module/ModuleHandler.class.php
+++ classes/module/ModuleHandler.class.php
@@ -1233,15 +1233,22 @@ class ModuleHandler extends Handler
 	function _setHttpStatusMessage($code)
 	{
 		$statusMessageList = array(
+			// 1×× Informational
 			'100' => 'Continue',
 			'101' => 'Switching Protocols',
-			'201' => 'OK', // todo check array key '201'
+			'102' => 'Processing',
+			// 2×× Success
+			'200' => 'OK',
 			'201' => 'Created',
 			'202' => 'Accepted',
-			'203' => 'Non-Authoritative Information',
+			'203' => 'Non-authoritative Information',
 			'204' => 'No Content',
 			'205' => 'Reset Content',
 			'206' => 'Partial Content',
+			'207' => 'Multi-Status',
+			'208' => 'Already Reported',
+			'226' => 'IM Used',
+			// 3×× Redirection
 			'300' => 'Multiple Choices',
 			'301' => 'Moved Permanently',
 			'302' => 'Found',
@@ -1249,6 +1256,8 @@ class ModuleHandler extends Handler
 			'304' => 'Not Modified',
 			'305' => 'Use Proxy',
 			'307' => 'Temporary Redirect',
+			'308' => 'Permanent Redirect',
+			// 4×× Client Error
 			'400' => 'Bad Request',
 			'401' => 'Unauthorized',
 			'402' => 'Payment Required',
@@ -1262,22 +1271,38 @@ class ModuleHandler extends Handler
 			'410' => 'Gone',
 			'411' => 'Length Required',
 			'412' => 'Precondition Failed',
-			'413' => 'Request Entity Too Large',
+			'413' => 'Payload Too Large',
 			'414' => 'Request-URI Too Long',
 			'415' => 'Unsupported Media Type',
 			'416' => 'Requested Range Not Satisfiable',
 			'417' => 'Expectation Failed',
+			'418' => 'I\'m a teapot',
+			'421' => 'Misdirected Request',
+			'422' => 'Unprocessable Entity',
+			'423' => 'Locked',
+			'424' => 'Failed Dependency',
+			'426' => 'Upgrade Required',
+			'428' => 'Precondition Required',
+			'429' => 'Too Many Requests',
+			'431' => 'Request Header Fields Too Large',
+			'451' => 'Unavailable For Legal Reasons',
+			// 5×× Server Error
 			'500' => 'Internal Server Error',
 			'501' => 'Not Implemented',
 			'502' => 'Bad Gateway',
 			'503' => 'Service Unavailable',
 			'504' => 'Gateway Timeout',
 			'505' => 'HTTP Version Not Supported',
+			'506' => 'Variant Also Negotiates',
+			'507' => 'Insufficient Storage',
+			'508' => 'Loop Detected',
+			'510' => 'Not Extended',
+			'511' => 'Network Authentication Required',
 		);
 		$statusMessage = $statusMessageList[$code];
 		if(!$statusMessage)
 		{
-			$statusMessage = 'OK';
+			$statusMessage = 'HTTP ' . $code;
 		}
 
 		Context::set('http_status_code', $code);
