fb5d72d40f118caca82e6915a39ed083a215e6cb
hemmxwxsoo <jbs09246@psoxs.com>
2017-02-12 19:32:43 +0900

    Fix Context.class.php

--- classes/context/Context.class.php
+++ classes/context/Context.class.php
@@ -452,11 +452,7 @@ class Context
 			return;
 		}
 
-		$config_file = $self->getConfigFile();
-		if(is_readable($config_file))
-		{
-			include($config_file);
-		}
+		include($self::getConfigFile());
 
 		// If master_db information does not exist, the config file needs to be updated
 		if(!isset($db_info->master_db))
