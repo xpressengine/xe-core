c76a8d54b29b2f7c6e44c1286798ceeb41a0bb96
bnu <bnufactory@gmail.com>
2017-02-21 17:01:07 +0900

    Fix #2035 XEVE-17-002 쉬운 설치 페이지에서 발생하는 보안 취약점 고침 - 제보자 : 최봉환(stayp05)

--- modules/autoinstall/autoinstall.admin.view.php
+++ modules/autoinstall/autoinstall.admin.view.php
@@ -482,7 +482,7 @@ class autoinstallAdminView extends autoinstall
 
 		$security = new Security();
 		$security->encodeHTML('package.', 'package.depends..', 'item_list..');
-		$security->encodeHTML('search_target', 'search_keyword');
+		$security->encodeHTML('search_target', 'search_keyword', 'order_target', 'order_type');
 	}
 
 	/**
