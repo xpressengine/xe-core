3d203245187247f1985adc0de8bd475f0987fd12
bnu <bnufactory@gmail.com>
2017-06-08 18:34:19 +0900

    #2099 forward 액션에 대한 csrf 검사 적용

--- classes/module/ModuleHandler.class.php
+++ classes/module/ModuleHandler.class.php
@@ -626,6 +626,18 @@ class ModuleHandler extends Handler
 
 				$xml_info = $oModuleModel->getModuleActionXml($forward->module);
 
+				// check CSRF for non-GET actions
+				$use_check_csrf = isset($xml_info->action->{$this->act}) && $xml_info->action->{$this->act}->check_csrf !== 'false';
+				if($use_check_csrf && $_SERVER['REQUEST_METHOD'] !== 'GET' && Context::isInstalled() && !checkCSRF())
+				{
+					$this->error = 'msg_invalid_request';
+					$oMessageObject = ModuleHandler::getModuleInstance('message', $display_mode);
+					$oMessageObject->setError(-1);
+					$oMessageObject->setMessage($this->error);
+					$oMessageObject->dispMessage();
+					return $oMessageObject;
+				}
+
 				// SECISSUE also check foward act method
 				// check REQUEST_METHOD in controller
 				if($type == 'controller')
