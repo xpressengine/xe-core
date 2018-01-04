46fd8d1468870d68edc715a958641c28f63fedd9
Kijin Sung <kijin@kijinsung.com>
2018-01-04 12:02:37 +0900

    Fix fatal error when using index hint with mysqli, mysqli_innodb, etc.

--- classes/xml/xmlquery/tags/table/HintTableTag.class.php
+++ classes/xml/xmlquery/tags/table/HintTableTag.class.php
@@ -37,7 +37,7 @@ class HintTableTag extends TableTag
 		$dbType = ucfirst(Context::getDBType());
 
 		$result = sprintf('new %sTableWithHint(\'%s\'%s, array('
-				, $dbType == 'Mysqli' ? 'Mysql' : $dbType
+				, !strncasecmp($dbType, 'Mysql', 5) ? 'Mysql' : $dbType
 				, $dbParser->escape($this->name)
 				, $this->alias ? ', \'' . $dbParser->escape($this->alias) . '\'' : ', null'
 				//, ', \'' . $dbParser->escape($this->index->name) .'\', \'' . $this->index->type .'\''
