d948babad9bdcc331e1256624dd694f55d6c4164
bnu <bnufactory@gmail.com>
2018-02-22 18:41:20 +0900

    #2218 #2223 PR에서 버그 수정 - 버그 수정 과정에서 동적으로 상태를 표시하는 코드를 제거하고 해당 페이지에서만 표시하도록 간소화 했습니다

--- modules/member/tpl/js/config.js
+++
@@ -1,9 +0,0 @@
-window.runOnce = false;
-function alertNewMemberAcceptSetting(isEnable, msg)
-{
-	if(window.runOnce) return;
-	window.runOnce = true;
-	var $ = jQuery;
-	$("#signupTab>a").text("⚠ " + $("#signupTab>a").text()).css("color", "#7F2000");
-	$(".x_msg-warn").show();
-}
