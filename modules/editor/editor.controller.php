0b833207929da9fd9b55210e167a7c16acf5db0e
Hwantae Ji <jht008@gmail.com>
2018-03-10 01:36:56 +0900

    에디터 컴포넌트가 디렉토리인지 확인

--- modules/editor/editor.controller.php
+++ modules/editor/editor.controller.php
@@ -487,6 +487,7 @@ class editorController extends editor
 		// Get xml_info of downloaded list
 		foreach($downloaded_list as $component_name)
 		{
+			if(!is_dir(_XE_PATH_.'modules/editor/components/'.$component_name)) continue;
 			if(in_array($component_name, array('colorpicker_text','colorpicker_bg'))) continue;
 			// Pass if configured
 			if($component_list->{$component_name}) continue;
