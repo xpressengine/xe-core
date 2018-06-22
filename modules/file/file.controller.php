f480fa109a845058a4a697775831515d950434a6
bnu <bnufactory@gmail.com>
2018-06-22 15:56:17 +0900

    #2008 `&`이 포함된 파일명의 이름이 변경되는 문제 고침

--- modules/file/file.controller.php
+++ modules/file/file.controller.php
@@ -712,6 +712,7 @@ class fileController extends file
 		$file_info['name'] = preg_replace('/\.(php|phtm|phar|html?|cgi|pl|exe|jsp|asp|inc)/i', '$0-x',$file_info['name']);
 		$file_info['name'] = removeHackTag($file_info['name']);
 		$file_info['name'] = str_replace(array('<','>'),array('%3C','%3E'),$file_info['name']);
+		$file_info['name'] = str_replace('&amp;', '&', $file_info['name']);
 
 		// Get random number generator
 		$random = new Password();
